9643920a17391732859b21657eca83289b798bad
Ekin Aslan <ekinaslan.js@gmail.com>
2024-07-28 21:00:51 +0300

    Fix

--- services/check.php
+++ services/check.php
@@ -7,8 +7,16 @@ $address = getWalletAddress(get_user_wallet_id($_GET["email"]));
 $flag = false;
 foreach (getTransactionList() as $transaction) {
     if ($transaction["sourceAddress"] == $address && $transaction["amounts"][0] == $_GET["price"]) {
-        $flag = true;
-        echo json_encode(["status" => "success"]);
+
+        $date = new DateTime($transaction["updateDate"], new DateTimeZone('UTC'));
+        $date = $date->getTimestamp() * 1000;
+
+        if (($date - $_GET["date"]) / (1000 * 60 * 60) < 12) { // 12 is for easying time zone check.
+            $flag = true;
+            echo json_encode(["status" => "success"]);
+            break;
+        }
+
     }
 }
 
